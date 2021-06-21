7195bcb05711db5eaa073b7a78b72e5c3a190168
Nehal Abubakar <nehalabubakar62@gmail.com>
2021-06-21 21:14:52 +0500

    Dashboard with all the functionalities
    Codeigniter dashboard with all the functionalities

--- Codeigniter/admin-dashboard/application/config/config.php
+++ Codeigniter/admin-dashboard/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/github/PHP/Codeigniter/codeigniter-boiler-plate/';
+$config['base_url'] = 'http://localhost/github/PHP/Codeigniter/admin-dashboard/';
 
 /*
 |--------------------------------------------------------------------------
